cb4c3c50a36283d909c5f6c970d2476dfd2a018a
shareysanr <shareysanr1@gmail.com>
2025-01-23 19:25:25 -0500

    Added output result and button to GUI

--- src/main/java/snipster/SnippetGUI.java
+++ src/main/java/snipster/SnippetGUI.java
@@ -2,6 +2,7 @@ package snipster;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
@@ -13,8 +14,17 @@ public class SnippetGUI extends Application {
         Label label = new Label("Input Snippet");
         TextField textField = new TextField();
 
+        Button button = new Button("Enter Snippet Title");
+        Label outputLabel = new Label();
+
+        button.setOnAction(e -> {
+            String input = textField.getText();
+            outputLabel.setText("Snippet Title: " + input);
+            textField.setText("");
+        });
+
         VBox layout = new VBox(10);
-        layout.getChildren().addAll(label, textField);
+        layout.getChildren().addAll(label, textField, button, outputLabel);
 
         Scene scene = new Scene(layout, 300, 200);
 
