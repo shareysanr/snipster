f76c2c95a85f6a79412a3e6360b8e70c6e942879
shareysanr <shareysanr1@gmail.com>
2025-01-23 19:05:41 -0500

    Updated GUI for inputting text field

--- src/main/java/snipster/SnippetGUI.java
+++ src/main/java/snipster/SnippetGUI.java
@@ -1,12 +1,25 @@
 package snipster;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class SnippetGUI extends Application {
     @Override
     public void start(Stage primaryStage) {
+        Label label = new Label("Input Snippet");
+        TextField textField = new TextField();
+
+        VBox layout = new VBox(10);
+        layout.getChildren().addAll(label, textField);
+
+        Scene scene = new Scene(layout, 300, 200);
+
         primaryStage.setTitle("Snippet GUI");
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
