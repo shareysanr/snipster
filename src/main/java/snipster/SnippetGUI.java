388161881087cb6b1af9de8066d78064d9b6902d
shareysanr <shareysanr1@gmail.com>
2025-01-23 19:41:49 -0500

    Added ListView for snippets added

--- src/main/java/snipster/SnippetGUI.java
+++ src/main/java/snipster/SnippetGUI.java
@@ -7,6 +7,9 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.ListView;
 
 public class SnippetGUI extends Application {
     @Override
@@ -17,14 +20,22 @@ public class SnippetGUI extends Application {
         Button button = new Button("Enter Snippet Title");
         Label outputLabel = new Label();
 
+        ObservableList<String> snippets = FXCollections.observableArrayList();
+        ListView<String> listView = new ListView<>(snippets);
+
         button.setOnAction(e -> {
-            String input = textField.getText();
-            outputLabel.setText("Snippet Title: " + input);
+            String input = textField.getText().trim();
+            if (input.equals("")) {
+                outputLabel.setText("Enter a valid snippet title");
+            } else {
+                outputLabel.setText("Snippet Title: " + input);
+                snippets.add(input);
+            }
             textField.setText("");
         });
 
         VBox layout = new VBox(10);
-        layout.getChildren().addAll(label, textField, button, outputLabel);
+        layout.getChildren().addAll(label, textField, button, outputLabel, listView);
 
         Scene scene = new Scene(layout, 300, 200);
 
