2d61e19fc606f639f7ac0559877afd6e3e92eae1
shareysanr <shareysanr1@gmail.com>
2025-02-11 21:27:43 -0500

    Added insert snippet Junit test

--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -1,10 +1,25 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import java.util.List;
+import snipster.SnippetRepository;
+import snipster.Snippet;
 
 public class SimpleTest {
     @Test
     public void testSimple() {
         int x = 1 + 1;
         Assertions.assertEquals(2, x);
+    }
+
+    @Test
+    public void testInsert() {
+        List<Snippet> snippets = SnippetRepository.readSnippets();
+        int size = snippets.size();
+
+        SnippetRepository.insertSnippet("Test", "print(\"Test\")", "Python");
+        snippets = SnippetRepository.readSnippets();
+
+        Assertions.assertEquals(size + 1, snippets.size());
+        Assertions.assertEquals("Test", snippets.getLast().getTitle());
     }
 }
