c3152c2021562ebaddb8fbdd8d1ff87e6247b8aa
shareysanr <shareysanr1@gmail.com>
2025-02-19 21:26:42 -0500

    Added test for checking export content

--- src/test/java/SnippetExportTest.java
+++ src/test/java/SnippetExportTest.java
@@ -1,4 +1,7 @@
 import org.junit.jupiter.api.Test;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import snipster.SnippetRepository;
@@ -7,7 +10,7 @@ import org.junit.jupiter.api.Assertions;
 public class SnippetExportTest {
 
     @Test
-    public void testCSVExportCreatesFile() {
+    public void testCSVExportFile() {
         String fileName = "test_snippets.csv";
 
         SnippetRepository.exportSnippets(fileName);
@@ -21,5 +24,23 @@ public class SnippetExportTest {
             System.out.println("Failed deleting file ");
             System.out.println(e.getMessage());
         }
+    }
+
+    @Test
+    public void testCSVExportContent() throws Exception {
+        String filePath = "test_snippets.csv";
+        SnippetRepository.exportSnippets(filePath);
+
+        Path path = Path.of(filePath);
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String header = reader.readLine();
+            Assertions.assertEquals("ID,Title,Code,Tags", header);
+
+            String firstSnippet = reader.readLine();
+            Assertions.assertNotNull(firstSnippet);
+        }
+
+        Files.delete(path);
     }
 }
